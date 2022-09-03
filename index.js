cb8b8b0dbb8703f2891b5fcfde9e6ab2e41c306b
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-09-03 10:40:42 +0200

    1

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-console.log("works")
+// 1
+function add(x, y) {
+  return x + y;
+}
+
+function sub(x, y) {
+  return x - y;
+}
+
+function mul(x, y) {
+  return x * y;
+}
+
+const identitiyf = (x) => {
+  return () => {
+    return x;
+  };
+};
+
+const five = identitiyf(5);
+// console.log(five());
+
+const addf = (x) => {
+  return (y) => {
+    return x + y;
+  };
+};
+
+// console.log(addf(5)(10));
+
+const liftf = (binaryFunc) => {
+  return (x) => {
+    return (y) => {
+      return binaryFunc(x, y);
+    };
+  };
+};
+
+const mulf = liftf(mul);
+console.log(mulf(10)(5));
