e1d5a3dcc560583c4888b97612f58a4423a6fe9a
Benedikt Koenig <benedikt.koenig@sinnerschrader.com>
2022-09-04 14:26:53 +0200

    2

--- index.js
+++ index.js
@@ -37,4 +37,26 @@ const liftf = (binaryFunc) => {
 };
 
 const mulf = liftf(mul);
-console.log(mulf(10)(5));
+// console.log(mulf(10)(5));
+
+// 2
+const curry = (binaryFunc, x) => {
+  return (y) => {
+    return binaryFunc(x, y);
+  };
+};
+
+// alternative
+// const curry = (binary, first) => {
+//   return liftf(binary)(first);
+// };
+
+// const add3 = curry(add, 3);
+
+// console.log(curry(add, 3)(7));
+
+const inc1 = addf(1);
+const inc2 = liftf(add)(1);
+const inc3 = curry(add, 1);
+
+// console.log(inc1(5));
